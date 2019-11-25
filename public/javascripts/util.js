6b1f96e045d551917912ab4b9e4078a350198514
blockfluter <randy@randallwiggins.com>
2019-11-24 23:59:47 -0600

    clear button

--- public/javascripts/util.js
+++ public/javascripts/util.js
@@ -1,5 +1,4 @@
-function copy(element, name)
-{
+function copy(element, name) {
 	document.getElementById(name).value = element.value;
 }
 
@@ -8,15 +7,21 @@ window.onload = (event) => {
 		Array.prototype.forEach.call(document.getElementsByClassName('status1'), e => {
 			e.classList.remove('status1');
 		});
-	},1000);
+	}, 1000);
+}
+
+function clearInputs() {
+	Array.prototype.forEach.call(document.getElementsByTagName('input'), e => {
+		e.value = '';
+	});
 }
 
 function loadRow(tr) {
 	Array.prototype.forEach.call(tr.getElementsByTagName('td'), e => {
-		document.getElementById(e.dataset.key).value = e.innerText;	
+		document.getElementById(e.dataset.key).value = e.innerText;
 	});
 	tr.classList.add('status1');
 	setTimeout(e => {
 		e.classList.remove('status1');
-	},800,tr)
+	}, 800, tr)
 }
